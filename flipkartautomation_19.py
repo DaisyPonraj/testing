465137ac79c5d7c9863011ea3cf396d26be4c7e0
Daisy <daisyponraj1101@gmail.com>
2018-06-20 12:03:34 +0530

    issue fixed

--- flipkartautomation_19.py
+++ flipkartautomation_19.py
@@ -7,7 +7,6 @@ from threading import Timer
 import time
 UN = "9894055756"
 PWD = "Psss@123"
-invalid_un = "8754232355"
 
 browser = webdriver.Firefox()
 browser.get('https://www.flipkart.com/')
@@ -17,18 +16,18 @@ password_x = '/html/body/div[2]/div/div/div/div/div[2]/div/form/div[2]/input'
 login_x = '/html/body/div[2]/div/div/div/div/div[2]/div/form/div[3]/button'
 def login():
     def invalid_un():
-        username = browser.find_element_by_xpath(username_x).send_keys(invalid_un)
+        username = browser.find_element_by_xpath(username_x).send_keys("8754232355")
         time.sleep(3)
-        password = browser.find_element_by_xpath(password_x).send_keys(PWD)
+        validpassword = browser.find_element_by_xpath(password_x).send_keys(PWD)
         login_into = browser.find_element_by_xpath(login_x).click()
         Timer(5.0,valid_un_pwd).start()
         # except:
         #     print("pass")
 
     def valid_un_pwd():
-        username = browser.find_element_by_xpath(username_x).send_keys(UN)
+        validusername = browser.find_element_by_xpath(username_x).send_keys(UN)
         time.sleep(3)
-        password = browser.find_element_by_xpath(password_x).send_keys(PWD)
+        validpassword = browser.find_element_by_xpath(password_x).send_keys(PWD)
         login_into = browser.find_element_by_xpath(login_x).click()
         print ("login successful")
         time.sleep(3)  
